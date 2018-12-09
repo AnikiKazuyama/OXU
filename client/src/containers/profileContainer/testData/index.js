5132e3b35b4c2c334cda6ab09ee9ae227912d9c5
Chilikov <chilikovvadim@gmail.com>
2018-12-10 01:18:09 +0300

    Add test data for table in bookmarks

--- client/src/containers/profileContainer/testData/index.js
+++ client/src/containers/profileContainer/testData/index.js
@@ -7,3 +7,111 @@ export default {
     max: 2440
   }
 };
+
+export const bookmarksData = {
+  favorite: {
+    title: 'Favorite',
+    items: [
+      {
+        title: 'The Breaker',
+        hero: '',
+        views: 24599,
+        volumes: 15,
+        chapters: 122,
+        current: 10,
+        preview: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/The_Breaker_%28manhwa%29_1_volume_Daiwon_C.I..jpg/220px-The_Breaker_%28manhwa%29_1_volume_Daiwon_C.I..jpg',
+        type: 'Manhwaa'
+      },
+      {
+        title: 'All You Need Is Kill',
+        hero: '',
+        views: 9857,
+        volumes: 1,
+        chapters: 1,
+        current: 0,
+        preview: 'https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Series/23855/23855._SX360_QL80_TTD_.jpg',
+        type: 'Manga'
+      },
+      {
+        title: 'The Tower of God',
+        hero: '',
+        views: 39827192,
+        volumes: 2,
+        chapters: 329,
+        current: 329,
+        preview: 'https://static.anidub.com/blog/2017/10/Grumpy_Tower_of_God_ch10_feature-1-.jpg',
+        type: 'Manhwaa'
+      },
+      {
+        title: 'Shamo',
+        hero: '',
+        views: 32570,
+        volumes: 34,
+        chapters: 334,
+        current: 334,
+        preview: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ7vK2oMrYnWKOT-k8EQPrAmX6hGQAeJMtNhC9Jasba6eKZCmq',
+        type: 'Manhwaa'
+      },
+      {
+        title: 'The Wedding Eve',
+        hero: '',
+        views: 6439,
+        volumes: 1,
+        chapters: 7,
+        current: 3,
+        preview: 'https://images-na.ssl-images-amazon.com/images/I/51usoeDAonL._SX341_BO1,204,203,200_.jpg',
+        type: 'Manga'
+      }
+    ]
+  },
+  reading: {
+    title: 'Reading',
+    items: [
+      {
+        title: 'Berserk',
+        hero: 'https://s3.anilist.co/media/manga/banner/2-wkmxrZ2d0Zx5.jpg',
+        views: 9591821,
+        volumes: 40,
+        chapters: 358,
+        current: 201,
+        preview: 'https://s3.anilist.co/media/manga/cover/medium/2-EpAB47R1L1Hh.jpg',
+        type: 'Manga'
+      },
+      {
+        title: 'One Piece',
+        hero: '',
+        views: 1226173,
+        volumes: 91,
+        chapters: 916,
+        current: 513,
+        preview: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg/220px-One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg',
+        type: 'Manga'
+      },
+      {
+        title: 'Bleach',
+        hero: '',
+        views: 872501,
+        volumes: 52,
+        chapters: 621,
+        current: 12,
+        preview: 'https://cdn.myanimelist.net/images/anime/3/40451.jpg',
+        type: 'Manga'
+      }
+    ]
+  },
+  done: {
+    title: 'Done',
+    items: [
+      {
+        title: 'Fairy Tail',
+        hero: '',
+        views: 52987,
+        volumes: 88,
+        chapters: 753,
+        current: 591,
+        preview: 'https://images.penguinrandomhouse.com/cover/9781632364203',
+        type: 'Manga'
+      }
+    ]
+  }
+};
