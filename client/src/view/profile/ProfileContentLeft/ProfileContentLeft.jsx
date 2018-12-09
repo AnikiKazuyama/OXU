f9ea7a897d2ed06816454386e5dca5a43dbec6db
Chilikov <chilikovvadim@gmail.com>
2018-12-10 01:09:36 +0300

    Return simple navItem

--- client/src/view/profile/ProfileContentLeft/ProfileContentLeft.jsx
+++ client/src/view/profile/ProfileContentLeft/ProfileContentLeft.jsx
@@ -6,23 +6,17 @@ import Card from '../../common/card';
 
 function ProfileContentLeft() {
   return (
-    <Card className="profile-page__content-left">
+    <Card className="profile-page__content-left" shadow>
       <Navigation horizontal>
         <NavItem
           exact
           id="achivments"
-          href="/im/achivments"
+          href="/im"
         >
           Achivments
         </NavItem>
-        <NavItem id="bookmarks">
-          <Accordion simple headerClassName="profile-menu" title="Bookmarks">
-            <Navigation horizontal>
-              <NavItem href="/im/bookmarks/favorite">Favorite</NavItem>
-              <NavItem href="/im/bookmarks/process">In process</NavItem>
-              <NavItem href="/im/bookmarks/done">Done</NavItem>
-            </Navigation>
-          </Accordion>
+        <NavItem id="bookmarks" href="/im/bookmarks">
+          Bookmarks
         </NavItem>
         <NavItem
           id="recentActivities"
