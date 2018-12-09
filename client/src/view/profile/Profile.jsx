0401ce85c2553f6f243ea60f838ae3fc92d233ed
Chilikov <chilikovvadim@gmail.com>
2018-12-10 00:56:22 +0300

    Move navigation from left panel to top

--- client/src/view/profile/Profile.jsx
+++ client/src/view/profile/Profile.jsx
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import ProfileHeader from './ProfileHeader';
-import ProfileContentLeft from './ProfileContentLeft';
+// import ProfileContentLeft from './ProfileContentLeft';
 import ProfileContentCenter from './ProfileContentCenter';
 
+import Card from '../common/card';
 import ProfileContent from '../common/panel';
 import Hero from '../common/hero';
+import Navigation, { NavItem } from '../common/navigation';
 
 import withWrapper from '../../HOC/withWrapper';
 
 import cloudsImage from '../../../public/imgs/clouds.jpg';
 import './style/index.scss';
 
-const ProfileContentWithWrapper = withWrapper(ProfileContent);
+const ProfileContentWithWrapper = withWrapper(ProfileContentCenter);
+const NavigationWithWrapper = withWrapper(Navigation);
 
 class Profile extends Component {
   static propTypes = {
@@ -31,10 +34,37 @@ class Profile extends Component {
       <div className="profile-page">
         <Hero url={cloudsImage} />
         <ProfileHeader />
+        <Card className="profile-page__navigation">
+          <NavigationWithWrapper>
+            <NavItem
+              exact
+              id="achivments"
+              href="/im"
+            >
+              Achivments
+            </NavItem>
+            <NavItem id="bookmarks" href="/im/bookmarks">
+              Bookmarks
+            </NavItem>
+            <NavItem
+              id="recentActivities"
+              href="/im/recent-activity"
+            >
+              Recent activity
+            </NavItem>
+            <NavItem
+              id="settings"
+              href="/im/settings"
+            >
+              Settings
+            </NavItem>
+          </NavigationWithWrapper>
+        </Card>
+
         <ProfileContentWithWrapper
           containerClassName="profile-page__content"
-          left={<ProfileContentLeft />}
-          center={<ProfileContentCenter />}
+          left={null}
+          center={<ProfileContentWithWrapper />}
         />
       </div>
     );
