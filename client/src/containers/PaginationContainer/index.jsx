19a896f133d3278ee47d6a6c4d87f5abd0957b0f
Chilikov <chilikovvadim@gmail.com>
2019-01-27 22:29:25 +0300

    Add pagination container

---
+++ client/src/containers/PaginationContainer/index.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { withRouter } from 'react-router-dom';
+
+import Pagination from '../../view/common/pagination';
+
+class PaginationContainer extends Component {
+  static defaultProps = {
+    className: ''
+  }
+
+  static propTypes = {
+    className: PropTypes.string
+  }
+
+  handleNavigation = (page) => {
+    const { match: { path, params, url }, history } = this.props;
+    let nextPath;
+
+    if (params.page) {
+      nextPath = path.replace(':page', page);
+    } else {
+      nextPath = `${url}/${page}`;
+    }
+
+    history.push(nextPath);
+  }
+
+  render() {
+    const { className, match: { params: { page = 1 } } } = this.props;
+
+    return (
+      <Pagination
+        className={className}
+        totalPages={12}
+        sibilingCount={1}
+        currentPage={Number(page)}
+        onNavigation={this.handleNavigation}
+      />
+    );
+  }
+}
+
+export default withRouter(PaginationContainer);
