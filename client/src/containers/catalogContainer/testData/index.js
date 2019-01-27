3c16c79e9614f5ee756d9c29caee1a0e3ffcdee4
Chilikov <chilikovvadim@gmail.com>
2019-01-27 20:33:23 +0300

    Add manga for testApi

--- client/src/containers/catalogContainer/testData/index.js
+++ client/src/containers/catalogContainer/testData/index.js
@@ -17,7 +17,7 @@ export default {
           count: 0
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/f/f8/Bokunopico.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/f/f8/Bokunopico.jpg',
       content: 'Boku no Pico (ぼくのぴこ Boku no Piko, lit. My Pico) is a Japanese series of shotacon anime OVAs produced by Natural High. The producer has described it as the worlds first Shotacon anime.[1] The series consists of three episodes, as well as a one-shot manga chapter, a computer game and a video compilation of songs. Because of the high cost of producing anime, the characters and contents were intensively product-tested before production began'
     },
     {
@@ -37,7 +37,7 @@ export default {
           count: 218957
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/94/NarutoCoverTankobon1.jpg/220px-NarutoCoverTankobon1.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/94/NarutoCoverTankobon1.jpg/220px-NarutoCoverTankobon1.jpg',
       content: 'is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, an adolescent ninja who searches for recognition from his peers and the village and also dreams of becoming the Hokage, the leader of his village. The story is in two parts, the first set in Narutos pre-teen years, and the second in his teens. The series is based on two one-shot manga by Kishimoto: Karakuri (1995), which earned Kishimoto an honorable mention in Shueishas monthly Hop Step Award the following year, and Naruto (1997).'
     },
     {
@@ -57,7 +57,7 @@ export default {
           count: 8292
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg/220px-One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg/220px-One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg',
       content: 'is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueishas Weekly Shōnen Jump magazine since July 22, 1997, and has been collected into 90 tankōbon volumes. The story follows the adventures of Monkey D. Luffy, a boy whose body gained the properties of rubber after unintentionally eating a Devil Fruit. With his crew of pirates, named the Straw Hat Pirates, Luffy explores the Grand Line in search of the worlds ultimate treasure known as One Piece in order to become the next Pirate King.'
     },
     {
@@ -77,7 +77,7 @@ export default {
           count: 0
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/f/f8/Bokunopico.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/f/f8/Bokunopico.jpg',
       content: 'Boku no Pico (ぼくのぴこ Boku no Piko, lit. My Pico) is a Japanese series of shotacon anime OVAs produced by Natural High. The producer has described it as the worlds first Shotacon anime.[1] The series consists of three episodes, as well as a one-shot manga chapter, a computer game and a video compilation of songs. Because of the high cost of producing anime, the characters and contents were intensively product-tested before production began'
     },
     {
@@ -97,7 +97,7 @@ export default {
           count: 218957
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/94/NarutoCoverTankobon1.jpg/220px-NarutoCoverTankobon1.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/94/NarutoCoverTankobon1.jpg/220px-NarutoCoverTankobon1.jpg',
       content: 'is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, an adolescent ninja who searches for recognition from his peers and the village and also dreams of becoming the Hokage, the leader of his village. The story is in two parts, the first set in Narutos pre-teen years, and the second in his teens. The series is based on two one-shot manga by Kishimoto: Karakuri (1995), which earned Kishimoto an honorable mention in Shueishas monthly Hop Step Award the following year, and Naruto (1997).'
     },
     {
@@ -117,7 +117,7 @@ export default {
           count: 8292
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg/220px-One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg/220px-One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg',
       content: 'is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueishas Weekly Shōnen Jump magazine since July 22, 1997, and has been collected into 90 tankōbon volumes. The story follows the adventures of Monkey D. Luffy, a boy whose body gained the properties of rubber after unintentionally eating a Devil Fruit. With his crew of pirates, named the Straw Hat Pirates, Luffy explores the Grand Line in search of the worlds ultimate treasure known as One Piece in order to become the next Pirate King.'
     },
     {
@@ -137,7 +137,7 @@ export default {
           count: 0
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/f/f8/Bokunopico.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/f/f8/Bokunopico.jpg',
       content: 'Boku no Pico (ぼくのぴこ Boku no Piko, lit. My Pico) is a Japanese series of shotacon anime OVAs produced by Natural High. The producer has described it as the worlds first Shotacon anime.[1] The series consists of three episodes, as well as a one-shot manga chapter, a computer game and a video compilation of songs. Because of the high cost of producing anime, the characters and contents were intensively product-tested before production began'
     },
     {
@@ -157,7 +157,7 @@ export default {
           count: 218957
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/94/NarutoCoverTankobon1.jpg/220px-NarutoCoverTankobon1.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/94/NarutoCoverTankobon1.jpg/220px-NarutoCoverTankobon1.jpg',
       content: 'is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, an adolescent ninja who searches for recognition from his peers and the village and also dreams of becoming the Hokage, the leader of his village. The story is in two parts, the first set in Narutos pre-teen years, and the second in his teens. The series is based on two one-shot manga by Kishimoto: Karakuri (1995), which earned Kishimoto an honorable mention in Shueishas monthly Hop Step Award the following year, and Naruto (1997).'
     },
     {
@@ -177,7 +177,7 @@ export default {
           count: 8292
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg/220px-One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg/220px-One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg',
       content: 'is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueishas Weekly Shōnen Jump magazine since July 22, 1997, and has been collected into 90 tankōbon volumes. The story follows the adventures of Monkey D. Luffy, a boy whose body gained the properties of rubber after unintentionally eating a Devil Fruit. With his crew of pirates, named the Straw Hat Pirates, Luffy explores the Grand Line in search of the worlds ultimate treasure known as One Piece in order to become the next Pirate King.'
     },
     {
@@ -197,7 +197,7 @@ export default {
           count: 0
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/f/f8/Bokunopico.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/f/f8/Bokunopico.jpg',
       content: 'Boku no Pico (ぼくのぴこ Boku no Piko, lit. My Pico) is a Japanese series of shotacon anime OVAs produced by Natural High. The producer has described it as the worlds first Shotacon anime.[1] The series consists of three episodes, as well as a one-shot manga chapter, a computer game and a video compilation of songs. Because of the high cost of producing anime, the characters and contents were intensively product-tested before production began'
     },
     {
@@ -217,7 +217,7 @@ export default {
           count: 218957
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/94/NarutoCoverTankobon1.jpg/220px-NarutoCoverTankobon1.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/94/NarutoCoverTankobon1.jpg/220px-NarutoCoverTankobon1.jpg',
       content: 'is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, an adolescent ninja who searches for recognition from his peers and the village and also dreams of becoming the Hokage, the leader of his village. The story is in two parts, the first set in Narutos pre-teen years, and the second in his teens. The series is based on two one-shot manga by Kishimoto: Karakuri (1995), which earned Kishimoto an honorable mention in Shueishas monthly Hop Step Award the following year, and Naruto (1997).'
     },
     {
@@ -237,7 +237,7 @@ export default {
           count: 8292
         }
       ],
-      source: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg/220px-One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg',
+      preview: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg/220px-One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg',
       content: 'is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueishas Weekly Shōnen Jump magazine since July 22, 1997, and has been collected into 90 tankōbon volumes. The story follows the adventures of Monkey D. Luffy, a boy whose body gained the properties of rubber after unintentionally eating a Devil Fruit. With his crew of pirates, named the Straw Hat Pirates, Luffy explores the Grand Line in search of the worlds ultimate treasure known as One Piece in order to become the next Pirate King.'
     }
   ]
