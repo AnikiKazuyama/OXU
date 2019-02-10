3a9ff638aed2c92a5f33ddc4f15d47d85d7ce88d
Chilikov <chilikovvadim@gmail.com>
2019-02-10 20:01:16 +0300

    Add method for first upper latter to StringUtils

--- client/src/utils/stringUtils/index.js
+++ client/src/utils/stringUtils/index.js
@@ -17,6 +17,10 @@ class StringUtils {
     }
     return result;
   }
+
+  static firstToUpper(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 }
 
 export default StringUtils;
