76df253f75470e33e1a80c8a24dff771bf1c3b7b
Chilikov <chilikovvadim@gmail.com>
2019-03-04 17:49:51 +0300

    Add manga module to core reducer

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -5,11 +5,13 @@ import modal from './modules/modal';
 import profile from './modules/profile';
 import bookmarks from './modules/bookmarks';
 import catalog from './modules/catalog';
+import manga from './modules/manga';
 
 export default history => combineReducers({
   router: connectRouter(history),
   modal,
   profile,
   bookmarks,
-  catalog
+  catalog,
+  manga
 });
